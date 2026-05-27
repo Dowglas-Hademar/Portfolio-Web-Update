4fa815d81982dc719bf136c6e64f5986bb68f5a5
Dowglas-Hademar <dowglashademar0@gmail.com>
2026-05-27 11:35:16 -0300

    Primeiro commit da segunda VA portfolio

--- portfolioweb/core/models.py
+++ portfolioweb/core/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 class Perfil(models.Model):
+    
+    # Vincula o perfil a um usuario do Django
+    usuario = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='perfil',
+        null=True,
+        blank=True,
+    )
+    
     nome = models.CharField(max_length= 100)
     descricao = models.TextField(max_length= 500)
     curso = models.CharField(max_length= 100)
@@ -9,6 +20,10 @@ class Perfil(models.Model):
     git = models.URLField(max_length= 400)
     linkedin = models.URLField(max_length= 400)
     imagem = models.ImageField(upload_to='perfil/', null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Perfil Pessoal'
+        verbose_name_plural = 'Perfis Pessoais'
     
     def __str__(self):
         return self.nome
